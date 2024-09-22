6b89a0eab32e537fc94f4bcef18bd8972b1b5bdb
Mananalik <mananmalik556e@gmail.com>
2024-09-22 22:11:40 +0530

    Nothing but headfile

--- src/manan1/Main.java
+++ src/manan1/Main.java
@@ -1,7 +1,29 @@
 package manan1;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    //       LL list = new LL();
+//       list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
+//        list.display();
+//        manan.Binarysearchcode.DLL list = new manan.Binarysearchcode.DLL();
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
+//        list.insertLast(99);
+//        list.insert(8,65);
+//        list.display();
+//        manan.BinarySearchDuplicate.CLL list = new manan.BinarySearchDuplicate.CLL();
+//        list.insert(23);
+//        list.insert(3);
+//        list.insert(19);
+//        list.insert(75);
+//        list.display();
+//        int x=0;
+//        int y=0;
+//        int z=0;
+//        int arr[] = {x,y,z};
+//        System.out.println(Arrays.toString(arr));
 }
