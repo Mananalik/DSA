579e94180228fe013279e0c2323d1990fdfa3fd7
Mananalik <mananmalik556e@gmail.com>
2024-09-29 09:51:23 +0530

    customStack

--- src/manan1/customStack.java
+++ src/manan1/customStack.java
@@ -1,5 +1,42 @@
 package manan1;
 
 public class customStack {
+  protected int [] data;
+  private static final int DEFAULT_SIZE=10;
+  int ptr = -1;
+  public customStack(){
+      this(DEFAULT_SIZE);
+  }
+  public customStack(int size){
+      this.data = new int[size];
+
+  }
+  public boolean push(int item){
+      if(isFull()){
+          System.out.println("Stack is full");
+          return false;
+      }
+      ptr++;
+      data[ptr] = item;
+      return true;
+  }
+  public int pop() throws Exception{
+      if(isEmpty()){
+          throw new Exception("Cannot pop from an empty stack!!");
+      }
+      return data[ptr--];
+  }
+  public int peek() throws Exception{
+      if(isEmpty()){
+          throw new Exception("Cannot pop from an empty stack!!");
+      }
+      return data[ptr];
+  }
+  public boolean isFull(){
+      return ptr==data.length-1;
+  }
+    public boolean isEmpty(){
+        return ptr==-1;
+    }
 
 }
