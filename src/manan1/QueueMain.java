ac85534f9b44bea7c0d7f3b2377ebc81bac65c6a
Mananalik <mananmalik556e@gmail.com>
2024-09-29 10:43:42 +0530

    CircularQueue

--- src/manan1/QueueMain.java
+++ src/manan1/QueueMain.java
@@ -2,7 +2,7 @@ package manan1;
 
 public class QueueMain {
     public static void main(String[] args) throws Exception {
-        CustomQueue queue  = new CustomQueue(5);
+        CircularQueue queue  = new CircularQueue(5);
         queue.insert(3);
         queue.insert(6);
         queue.insert(5);
@@ -10,6 +10,7 @@ public class QueueMain {
         queue.insert(1);
         queue.display();
         System.out.println(queue.remove());
+        queue.insert(133);
         queue.display();
     }
 }
