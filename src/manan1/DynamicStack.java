9c16225991f1176f77482f4ece086b53a253d3aa
Mananalik <mananmalik556e@gmail.com>
2024-09-29 10:05:06 +0530

    DynamicStack

---
+++ src/manan1/DynamicStack.java
@@ -0,0 +1,23 @@
+package manan1;
+
+public class DynamicStack extends customStack{
+    public DynamicStack(){
+        super();
+    }
+    public DynamicStack(int size){
+        super(size);
+    }
+    @Override
+    public boolean push(int item){
+        if(this.isFull()){
+            // double the array size
+            int[] temp = new int[data.length*2];
+            for(int i=0;i<data.length;i++){
+                temp[i]=data[i];
+            }
+            data=temp;
+        }
+        // insert item
+        return super.push(item);
+    }
+}
